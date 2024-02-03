fabfda1da90a35e6debca71995ca8a4015b3fcc3
Shrimad Bhagwat <51125208+Shrimad-Bhagwat@users.noreply.github.com>
2024-02-03 21:18:03 +0530

    new_update -v1.2.0

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:excel/excel.dart';
 import 'package:expense_tracker/main.dart';
 import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
-
 import '../models/expense.dart';
 import '../models/expenses_storage.dart';
 
@@ -13,14 +16,39 @@ class Expenses extends StatefulWidget {
   @override
   State<Expenses> createState() => _ExpensesState();
 }
+
 List<Expense> _registeredExpenses = [];
+List<String> months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
 class _ExpensesState extends State<Expenses> {
   final ExpensesStorage _expensesStorage = ExpensesStorage();
+
   @override
   void initState() {
     super.initState();
 
     _loadExpenses();
+  }
+
+  double calculateTotalExpense() {
+    double total = 0;
+    for (Expense expense in _registeredExpenses) {
+      total += expense.amount;
+    }
+    return total;
   }
 
   Future<void> _loadExpenses() async {
@@ -48,41 +76,30 @@ class _ExpensesState extends State<Expenses> {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         duration: const Duration(seconds: 3),
-        content: const Text('Expense Deleted'),
-        action: SnackBarAction(label: 'Undo', onPressed: (){
-          setState(() {
-            _registeredExpenses.insert(expenseIndex, expense);
-          });
-          _expensesStorage.saveExpenses(_registeredExpenses);
-        }),
+        backgroundColor: kColorScheme.primaryContainer,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            )),
+        content: const Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Text('Expense Deleted', style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),),
+        ),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _registeredExpenses.insert(expenseIndex, expense);
+              });
+              _expensesStorage.saveExpenses(_registeredExpenses);
+            }),
       ),
     );
   }
-
-  // void _addExpense(Expense expense) {
-  //   setState(() {
-  //     _registeredExpenses.add(expense);
-  //   });
-  // }
-  //
-  // void _removeExpense(Expense expense) {
-  //   final expenseIndex = _registeredExpenses.indexOf(expense);
-  //   setState(() {
-  //     _registeredExpenses.remove(expense);
-  //   });
-  //   ScaffoldMessenger.of(context).clearSnackBars();
-  //   ScaffoldMessenger.of(context).showSnackBar(
-  //     SnackBar(
-  //       duration: const Duration(seconds: 3),
-  //       content: const Text('Expense Deleted'),
-  //       action: SnackBarAction(label: 'Undo', onPressed: (){
-  //         setState(() {
-  //           _registeredExpenses.insert(expenseIndex, expense);
-  //         });
-  //       }),
-  //     ),
-  //   );
-  // }
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
@@ -98,7 +115,6 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    // print(width);
 
     Widget mainContent = const Center(
       child: Text('No expenses found!'),
@@ -111,23 +127,151 @@ class _ExpensesState extends State<Expenses> {
     }
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expense Tracker'),
+        title: const Text('Expense Tracker'),
         actions: [
+          IconButton(
+              onPressed: () {
+                showDownloadAlert();
+                // exportToExcel(_registeredExpenses, context);
+              },
+              icon: const Icon(Icons.download_rounded)),
           IconButton(
               onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add)),
         ],
       ),
-      body: width <600 ? Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(child: mainContent),
-        ],
-      ) : Row(
-        children: [
-          Expanded(child: Chart(expenses: _registeredExpenses)),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: mainContent),
+                // Bottom bar with total expense
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    height: 70,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        )),
+                    padding: const EdgeInsets.all(16),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Center(
+                        child: Text(
+                          'Total Expense : ₹ ${calculateTotalExpense()}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _registeredExpenses)),
+                Expanded(child: mainContent),
+              ],
+            ),
+      // floatingActionButton: FloatingActionButton(onPressed: (){}, child: Icon(Icons.add),),
     );
   }
+
+  void showDownloadAlert() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Export to Excel'),
+          content: const Text('Are you sure you want to export expenses to Excel?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context).pop(); // Close the dialog
+                await exportToExcel(_registeredExpenses, context);
+              },
+              child: const Text('Export'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+Future<void> exportToExcel(List<Expense> expenses, BuildContext context) async {
+  // Create an Excel workbook and add a worksheet
+  final Excel excel = Excel.createExcel();
+  final Sheet sheet = excel['Expense Details'];
+
+  // Add headers to the worksheet
+  sheet.appendRow([
+    const TextCellValue('Date'),
+    const TextCellValue('Description'),
+    const TextCellValue('Category'),
+    const TextCellValue('Amount'),
+  ]);
+
+  // Add expense data to the worksheet
+  for (Expense expense in expenses) {
+    var formattedDate =
+        "${expense.date.day}-${expense.date.month}-${expense.date.year}";
+    var category = expense.category.toString().split('.')[1].toUpperCase();
+    // print("$formattedDate $category");
+    sheet.appendRow([
+      TextCellValue(formattedDate),
+      TextCellValue(expense.title.toString()),
+      TextCellValue(category),
+      TextCellValue(expense.amount.toString()),
+    ]);
+  }
+
+  // Save the Excel file
+
+  try {
+    var month = DateTime.now().month;
+    final file =
+        File('/storage/emulated/0/Download/${months[month - 1]}_expenses.xlsx');
+    file.writeAsBytes(excel.encode()!);
+    ScaffoldMessenger.of(context).showSnackBar(
+        showMessage("Excel exported to Downloads!", Colors.green));
+
+  } on Exception catch (_) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(
+        showMessage("Error!", Colors.red[200]!)
+    );
+  }
+}
+
+SnackBar showMessage(String title, Color color) {
+  return SnackBar(
+      duration: const Duration(seconds: 3),
+      backgroundColor: color,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(20),
+        topRight: Radius.circular(20),
+      )),
+      content: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ));
 }
